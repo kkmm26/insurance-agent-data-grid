78c14f5f327240c05c61f552afa531cf13653b58
Kaung Khant Mann Maung <itp26kkmm@gmail.com>
2024-10-29 16:34:22 +0100

    implement total commission header

--- src/components/TotalCommision.tsx
+++ src/components/TotalCommision.tsx
@@ -1,10 +1,14 @@
+import { useTable } from "@/providers/TableProvider";
 
 function TotalCommision() {
+
+    const {table} = useTable();
+    const totalCommision = table.getFilteredRowModel().rows.reduce((acc, row) => acc + row.original.commissionAmount, 0);
+    const formattedTotalCommision = totalCommision.toLocaleString("en-US", { style: "currency", currency: "MMK" });
     return (
         <div className="bg-white p-4 rounded-lg shadow">
-            <h2 className="text-lg font-semibold">Total Comission</h2>
-            <p className="text-3xl font-bold">$12.1k+</p>
-            <p className="text-sm text-green-600">+20.1% increase last month</p>
+            <h2 className="text-lg font-semibold text-gray-500">Total Comission</h2>
+            <p className="text-3xl font-semibold text-gray-500">{formattedTotalCommision}</p>
         </div>
     );
 }
